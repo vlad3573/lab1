160a4f08c7b616546fbdf267595cdbe2f57f8384
vlad3573 <115645810+vlad3573@users.noreply.github.com>
2022-10-13 22:11:00 +0300

    Add files via upload

---
+++ Lab1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hello World! It took 6h! Good bye America!")
+        }
+    }
+}
